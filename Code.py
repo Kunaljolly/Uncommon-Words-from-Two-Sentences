29088710f737ecc2091801ef48a05b06b56571b9
Kunaljolly <134881423+Kunaljolly@users.noreply.github.com>
2024-09-17 16:00:01 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        count1 = Counter(s1.split(" "))
+        count2 = Counter(s2.split(" "))
+        set1 = [w1 for w1 in count1.keys() if count1[w1] <= 1 and count2[w1] <= 1]
+        set2 = [w2 for w2 in count2.keys() if count1[w2] <= 1 and count2[w2] <= 1]
+
+        return list(set(set1) - set(set2)) + list(set(set2) - set(set1))
